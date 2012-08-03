f847e4313ddd1ac5827507f892bf2ef948b982c9
Fernando Braz <fernando@fernando-braz.(none)>
2012-08-03 14:47:39 -0300

    More Validation test - final Task B

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -34,16 +34,26 @@ class ProductTest < ActiveSupport::TestCase
 
 	end
 		
-		test "image url" do
-			ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
-			http://a.b.c/x/y/z/fred.gif }
-			bad = %w{ fred.doc fred.gif/more fred.gif.more }
+	test "image url" do
+		ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+		http://a.b.c/x/y/z/fred.gif }
+		bad = %w{ fred.doc fred.gif/more fred.gif.more }
 
-			ok.each do |name|
-				assert new_product(name).valid?, "#{name} shouldn't be invalid"
-			end
-			bad.each do |name|
-				assert new_product(name).invalid?, "#{name} shouldn't be valid"
-			end
+		ok.each do |name|
+			assert new_product(name).valid?, "#{name} shouldn't be invalid"
 		end
+		bad.each do |name|
+			assert new_product(name).invalid?, "#{name} shouldn't be valid"
+		end
+	end
+
+	test "product is not valid without a unique title" do
+		product = Product.new(:title => products(:ruby).title,
+			:description => "yyy",
+			:price => 1,
+			:image_url => "fred.gif")
+
+		asssert !product.save
+		assert_equal "has already been taken", product.errors[:title].join('; ')
+	end
 end
